11ecd49ad00147739c600282fe0b7affb695e0ec
Drew Youngwerth <drew@youngwerth.com>
2020-09-17 13:32:15 -0600

    Add CLI command for outputing AST.

--- src/cli.ts
+++ src/cli.ts
@@ -3,14 +3,21 @@ import { createCommand } from "commander";
 import { readFileSync } from "fs";
 import { run } from "./run";
 import { compile } from "./compiler";
+import { parse } from "./parser";
 
 const program = createCommand();
 
 program
     .version(JSON.parse(readFileSync(`${__dirname}/../package.json`, { encoding: "utf8" })))
     .arguments("<file.dm>")
-    .action((file: string) => {
+    .option("-a, --ast", "Output parser ast only.")
+    .action((file: string, opts) => {
         const code = readFileSync(file, { encoding: "utf8" });
+        if (opts.ast) {
+            console.log(JSON.stringify(parse(code), undefined, 4))
+            return;
+        }
+
         const mod = compile(code);
         run(mod);
     });
