c83d93ed8ba99178907789850e74cdaefafc2490
Drew Youngwerth <drew@youngwerth.com>
2024-08-31 16:25:17 -0700

    Switch to vitest (#29)
    * Use vitest
    
    * Improve pr test script, no need to build anymore
    
    * Update snaps

--- src/semantics/__tests__/regular-macros.test.ts
+++ src/semantics/__tests__/regular-macros.test.ts
@@ -1,25 +1,18 @@
-import { describe, it } from "node:test";
 import { parse } from "../../parser/parser.js";
-import assert from "node:assert";
 import path from "node:path";
 import { registerModules } from "../modules.js";
 import { expandRegularMacros } from "../regular-macros.js";
 import { regularMacrosVoidFile } from "./fixtures/regular-macros-void-file.js";
-import { regularMacrosAst } from "./fixtures/regular-macros-ast.js";
+import { test } from "vitest";
 
-describe("regular macro evaluation", () => {
-  it("should evaluate macros of the example file", async () => {
-    const parserOutput = parse(regularMacrosVoidFile);
-    const files = { test: parserOutput };
-    const resolvedModules = registerModules({
-      files,
-      srcPath: path.dirname("test"),
-      indexPath: "test.void",
-    });
-    const result = expandRegularMacros(resolvedModules);
-    assert.deepStrictEqual(
-      JSON.parse(JSON.stringify(result)),
-      regularMacrosAst
-    );
+test("regular macro expansion", async (t) => {
+  const parserOutput = parse(regularMacrosVoidFile);
+  const files = { test: parserOutput };
+  const resolvedModules = registerModules({
+    files,
+    srcPath: path.dirname("test"),
+    indexPath: "test.void",
   });
+  const result = expandRegularMacros(resolvedModules);
+  t.expect(result).toMatchSnapshot();
 });
