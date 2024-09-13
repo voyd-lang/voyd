301a0e226c2fca26854cc0f17e70858ba7b7d0d1
Drew Youngwerth <drew@youngwerth.com>
2024-09-13 13:10:45 -0700

    Simplify child list

--- src/syntax-objects/lib/child-list.ts
+++ src/syntax-objects/lib/child-list.ts
@@ -1,10 +1,6 @@
 import { FastShiftArray } from "../../lib/fast-shift-array.js";
 import { Expr } from "../expr.js";
-import { Id, Identifier } from "../identifier.js";
-import { List } from "../list.js";
 import { NamedEntity } from "../named-entity.js";
-import { Syntax } from "../syntax.js";
-import { getIdStr } from "./get-id-str.js";
 
 export class ChildList<T extends Expr = Expr> {
   private store: FastShiftArray<T> = new FastShiftArray();
@@ -49,64 +45,16 @@ export class ChildList<T extends Expr = Expr> {
     return this.store.at(index);
   }
 
-  exprAt(index: number): Expr {
-    const expr = this.store.at(index);
-    if (!expr) {
-      throw new Error(`No expr at ${index}`);
-    }
-    return expr;
-  }
-
-  identifierAt(index: number): Identifier {
-    const id = this.at(index);
-    if (!id?.isIdentifier()) {
-      throw new Error(`No identifier at index ${index}`);
-    }
-    return id;
-  }
-
-  optionalIdentifierAt(index: number): Identifier | undefined {
-    const id = this.at(index);
-    if (id?.isIdentifier()) {
-      return id;
-    }
-  }
-
-  listAt(index: number): List {
-    const id = this.at(index);
-    if (!id?.isList()) {
-      throw new Error(`No list at index ${index}`);
-    }
-    return id;
-  }
-
   set(index: number, expr: T) {
     this.registerExpr(expr);
     this.store.set(index, expr);
     return this;
   }
 
-  calls(fnId: Id, atIndex = 0) {
-    return this.getIdStrAt(atIndex) === getIdStr(fnId);
-  }
-
-  getIdStrAt(index: number): string | undefined {
-    const v = this.at(index);
-    return v?.isIdentifier() || v?.isStringLiteral() ? v.value : undefined;
-  }
-
   consume(): T {
     const next = this.store.shift();
     if (!next) throw new Error("No remaining expressions");
     return next;
-  }
-
-  first(): T | undefined {
-    return this.store.at(0);
-  }
-
-  last(): T | undefined {
-    return this.store.at(-1);
   }
 
   /** Returns all but the first element in an array */
@@ -126,10 +74,6 @@ export class ChildList<T extends Expr = Expr> {
     return this;
   }
 
-  findIndex(cb: (expr: Expr) => boolean) {
-    return this.toArray().findIndex(cb);
-  }
-
   insert(expr: T, at = 0) {
     this.registerExpr(expr);
     this.store.splice(at, 0, expr);
@@ -141,17 +85,9 @@ export class ChildList<T extends Expr = Expr> {
     return this;
   }
 
-  filter(fn: (expr: T, index: number, array: T[]) => boolean): ChildList<T> {
-    return new ChildList(this.toArray().filter(fn), this.parent);
-  }
-
   each(fn: (expr: T, index: number, array: T[]) => void): ChildList<T> {
     this.toArray().forEach(fn);
     return this;
-  }
-
-  map(fn: (expr: T, index: number, array: T[]) => T): ChildList<T> {
-    return new ChildList(this.toArray().map(fn), this.parent);
   }
 
   applyMap(fn: (expr: T, index: number, array: T[]) => T): ChildList<T> {
@@ -161,27 +97,7 @@ export class ChildList<T extends Expr = Expr> {
     return this;
   }
 
-  /** Like a regular map, but omits undefined values returned from the mapper */
-  mapFilter(
-    fn: (expr: T, index: number, array: T[]) => T | undefined
-  ): ChildList<T> {
-    const list = new ChildList([], this.parent);
-    return this.toArray().reduce(
-      (newList: ChildList<T>, expr, index, array) => {
-        if (!expr) return newList;
-        const result = fn(expr, index, array);
-        if (!result) return newList;
-        return newList.push(result);
-      },
-      list
-    );
-  }
-
-  slice(start?: number, end?: number): ChildList<T> {
-    return new ChildList(this.store.slice(start, end), this.parent);
-  }
-
-  sliceAsArray(start?: number, end?: number) {
+  slice(start?: number, end?: number): T[] {
     return this.store.slice(start, end);
   }
 
@@ -202,7 +118,7 @@ export class ChildList<T extends Expr = Expr> {
     return this.store.toArray();
   }
 
-  toClonedArray(parent?: Expr): T[] {
+  toClonedArray(): T[] {
     return this.toArray().map((expr: T): T => expr.clone() as T);
   }
 
