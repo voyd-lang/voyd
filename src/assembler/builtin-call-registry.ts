cb6a6beed9756ce714a5d2430677f64afbec445e
Drew Youngwerth <drew@youngwerth.com>
2025-08-12 12:58:02 -0700

    refactor: dispatch built-in calls via registry (#97)
    * feat: dispatch built-in calls via registry
    
    * refactor: organize builtin compilers

---
+++ src/assembler/builtin-call-registry.ts
@@ -0,0 +1,33 @@
+import { CompileExprOpts, compileExpression } from "../assembler.js";
+import { Call } from "../syntax-objects/call.js";
+import { compileAssign } from "./builtin-calls/compile-assign.js";
+import { compileIf } from "./builtin-calls/compile-if.js";
+import { compileExport } from "./builtin-calls/compile-export.js";
+import { compileMemberAccess } from "./builtin-calls/compile-member-access.js";
+import { compileWhile } from "./builtin-calls/compile-while.js";
+import { compileFixedArray } from "./builtin-calls/compile-fixed-array.js";
+import { compileBinaryen } from "./builtin-calls/compile-binaryen.js";
+
+export type CallCompiler = (opts: CompileExprOpts<Call>) => number;
+
+export const builtinCallCompilers = new Map<string, CallCompiler>([
+  ["quote", (opts) => (opts.expr.argAt(0) as any).value],
+  ["=", compileAssign],
+  ["if", compileIf],
+  ["export", compileExport],
+  ["mod", (opts) => opts.mod.nop()],
+  ["member-access", compileMemberAccess],
+  ["while", compileWhile],
+  ["break", (opts) => opts.mod.br(opts.loopBreakId!)],
+  ["FixedArray", compileFixedArray],
+  ["binaryen", compileBinaryen],
+  [
+    ":",
+    (opts) =>
+      compileExpression({
+        ...opts,
+        expr: opts.expr.argAt(1)!,
+        isReturnExpr: opts.isReturnExpr,
+      }),
+  ],
+]);
---
+++ src/assembler/builtin-calls/compile-fixed-array.ts
@@ -0,0 +1,13 @@
+import { CompileExprOpts, compileExpression, mapBinaryenType } from "../../assembler.js";
+import { Call } from "../../syntax-objects/call.js";
+import { FixedArrayType } from "../../syntax-objects/types.js";
+import * as gc from "../../lib/binaryen-gc/index.js";
+
+export const compileFixedArray = (opts: CompileExprOpts<Call>) => {
+  const type = opts.expr.type as FixedArrayType;
+  return gc.arrayNewFixed(
+    opts.mod,
+    gc.binaryenTypeToHeapType(mapBinaryenType(opts, type)),
+    opts.expr.argArrayMap((expr) => compileExpression({ ...opts, expr }))
+  );
+};
