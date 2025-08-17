b7ef57151c8e1207edad905469f83c44c189d9dd
Drew Youngwerth <drew@youngwerth.com>
2025-08-16 23:03:52 -0700

    feat: infer closure parameter types

---
+++ src/__tests__/closure-param-infer.e2e.test.ts
@@ -0,0 +1,26 @@
+import {
+  closureParamInferOneVoyd,
+  closureParamInferTwoVoyd,
+} from "./fixtures/closure-param-infer.js";
+import { compile } from "../compiler.js";
+import { describe, test } from "vitest";
+import assert from "node:assert";
+import { getWasmFn, getWasmInstance } from "../lib/wasm.js";
+
+describe("E2E closure parameter inference", () => {
+  test("infers single parameter closure", async (t) => {
+    const mod = await compile(closureParamInferOneVoyd);
+    const instance = getWasmInstance(mod);
+    const fn = getWasmFn("main", instance);
+    assert(fn, "Function exists");
+    t.expect(fn(), "main returns correct value").toEqual(10);
+  });
+
+  test("infers two parameter closure", async (t) => {
+    const mod = await compile(closureParamInferTwoVoyd);
+    const instance = getWasmInstance(mod);
+    const fn = getWasmFn("main", instance);
+    assert(fn, "Function exists");
+    t.expect(fn(), "main returns correct value").toEqual(7);
+  });
+});
