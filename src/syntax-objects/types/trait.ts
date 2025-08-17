a214621c3e1ff96d39f69bba228438e1ab41134e
Drew Youngwerth <drew@youngwerth.com>
2025-08-17 00:19:05 -0700

    feat: add generic trait support

--- src/syntax-objects/types/trait.ts
+++ src/syntax-objects/types/trait.ts
@@ -6,11 +6,14 @@ import { Implementation } from "../implementation.js";
 import { BaseType } from "./base-type.js";
 import { ScopedEntity } from "../scoped-entity.js";
 import { LexicalContext } from "../lib/lexical-context.js";
-import { TypeJSON } from "../types.js";
+import { Type, TypeJSON } from "../types.js";
+import { Identifier } from "../identifier.js";
 
 export type TraitOpts = ScopedNamedEntityOpts & {
   methods: Fn[];
   lexicon?: LexicalContext;
+  typeParameters?: Identifier[];
+  implementations?: Implementation[];
 };
 
 export class TraitType extends BaseType implements ScopedEntity {
@@ -18,21 +21,37 @@ export class TraitType extends BaseType implements ScopedEntity {
   readonly methods: ChildList<Fn>;
   readonly lexicon: LexicalContext;
   implementations: Implementation[] = [];
+  typeParameters?: Identifier[];
+  appliedTypeArgs?: Type[];
+  genericInstances?: TraitType[];
+  genericParent?: TraitType;
+  typesResolved?: boolean;
+  #iteration = 0;
 
   constructor(opts: TraitOpts) {
     super(opts);
     this.lexicon = opts.lexicon ?? new LexicalContext();
     this.methods = new ChildList(opts.methods, this);
+    this.typeParameters = opts.typeParameters;
+    this.implementations = opts.implementations ?? [];
   }
 
   clone(parent?: Expr): TraitType {
     return new TraitType({
       ...super.getCloneOpts(parent),
+      id: `${this.id}#${this.#iteration++}`,
       methods: this.methods.clone(),
+      typeParameters: this.typeParameters,
+      implementations: [],
     });
   }
 
   toJSON(): TypeJSON {
     return ["type", ["trait", this.name, ["methods", this.methods.toJSON()]]];
+  }
+
+  registerGenericInstance(trait: TraitType) {
+    if (!this.genericInstances) this.genericInstances = [];
+    this.genericInstances.push(trait);
   }
 }
