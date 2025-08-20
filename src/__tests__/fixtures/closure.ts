a57e295928841516eceb36c40d2d7c10922095c3
Drew Youngwerth <drew@youngwerth.com>
2025-08-20 16:30:34 -0700

    test: consolidate closure and trait object e2e tests

--- src/__tests__/fixtures/closure.ts
+++
@@ -1,8 +0,0 @@
-export const closureVoyd = `
-use std::all
-
-pub fn run() -> i32
-  let x = 41
-  let add_closure = (y: i32) => x + y
-  add_closure(1)
-`;
--- src/__tests__/fixtures/pipe-closure.ts
+++
@@ -1,8 +0,0 @@
-export const pipeClosureVoyd = `
-use std::all
-
-pub fn main() -> i32
-  2
-    .(x: i32) => x + 3
-    .(x: i32) => x * 5
-`;
