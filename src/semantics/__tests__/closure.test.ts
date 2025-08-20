47f941fad82937fbeea36d4c422ceeb74e6bd08e
Drew Youngwerth <drew@youngwerth.com>
2025-08-19 22:57:04 -0700

    fix: capture callee identifiers

--- src/semantics/__tests__/closure.test.ts
+++ src/semantics/__tests__/closure.test.ts
@@ -1,11 +1,13 @@
 import { parse } from "../../parser/parser.js";
 import { Block } from "../../syntax-objects/block.js";
+import { Call } from "../../syntax-objects/call.js";
 import { Identifier } from "../../syntax-objects/identifier.js";
 import { Closure } from "../../syntax-objects/closure.js";
 import { Fn } from "../../syntax-objects/fn.js";
 import { Parameter } from "../../syntax-objects/parameter.js";
 import { Variable } from "../../syntax-objects/variable.js";
 import { Int } from "../../syntax-objects/int.js";
+import { List } from "../../syntax-objects/list.js";
 import { initEntities } from "../init-entities.js";
 import { resolveEntities } from "../resolution/resolve-entities.js";
 import { test } from "vitest";
@@ -43,6 +45,22 @@ test("closure captures parameter from enclosing function", (t) => {
   const closure = new Closure({
     body: new Block({ body: [Identifier.from("x")] }),
   });
+  const fn = new Fn({
+    name: Identifier.from("outer"),
+    parameters: [param],
+    body: new Block({ body: [closure] }),
+  });
+
+  resolveEntities(closure);
+
+  t.expect(closure.captures.length).toBe(1);
+  t.expect(closure.captures[0]).toBe(param);
+});
+
+test("closure captures parameter used as callee", (t) => {
+  const param = new Parameter({ name: Identifier.from("cb") });
+  const call = new Call({ fnName: Identifier.from("cb"), args: new List({ value: [] }) });
+  const closure = new Closure({ body: new Block({ body: [call] }) });
   const fn = new Fn({
     name: Identifier.from("outer"),
     parameters: [param],
