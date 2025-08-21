b14e7408be13b2a0714f1db9f4bfb23333f932ed
Drew Youngwerth <drew@youngwerth.com>
2025-08-20 18:32:05 -0700

    Expose JSON memory helpers

--- src/lib/wasm.ts
+++ src/lib/wasm.ts
@@ -1,13 +1,14 @@
 import binaryen from "binaryen";
 
 export const getWasmInstance = (
-  mod: Uint8Array | binaryen.Module
+  mod: Uint8Array | binaryen.Module,
+  imports?: WebAssembly.Imports
 ): WebAssembly.Instance => {
   const bin = (
     mod instanceof Uint8Array ? mod : mod.emitBinary()
   ) as unknown as BufferSource;
   const compiled = new WebAssembly.Module(bin);
-  return new WebAssembly.Instance(compiled);
+  return new WebAssembly.Instance(compiled, imports);
 };
 
 export const getWasmFn = (
---
+++ src/types/msgpack.d.ts
@@ -0,0 +1,2 @@
+declare module "@msgpack/msgpack";
+
