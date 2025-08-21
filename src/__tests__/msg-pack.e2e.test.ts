ee57ccc1cdd46b5e7a13b8a875270d9a4e60e457
Drew Youngwerth <drew@youngwerth.com>
2025-08-21 16:53:51 -0700

    feat: encode strings in msg pack (#176)
    * feat: add string encoding to msg pack
    
    * Actually decode the strings

--- src/__tests__/msg-pack.e2e.test.ts
+++ src/__tests__/msg-pack.e2e.test.ts
@@ -3,6 +3,7 @@ import { compile } from "../compiler.js";
 import { describe, test } from "vitest";
 import assert from "node:assert";
 import { getWasmFn, getWasmInstance } from "../lib/wasm.js";
+import { decode } from "@msgpack/msgpack";
 
 describe("E2E msg pack encode", () => {
   test("encodes number into memory", async (t) => {
@@ -11,5 +12,15 @@ describe("E2E msg pack encode", () => {
     const fn = getWasmFn("run", instance);
     assert(fn, "Function exists");
     t.expect(fn(), "encoded number written").toEqual(42);
+  });
+
+  test("encodes string into memory", async (t) => {
+    const mod = await compile(msgPackVoyd);
+    const instance = getWasmInstance(mod);
+    const fn = getWasmFn("run_string", instance);
+    assert(fn, "Function exists");
+    fn();
+    const memory = instance.exports["main_memory"] as WebAssembly.Memory;
+    t.expect(decode(memory.buffer.slice(0, 4))).toEqual("abc");
   });
 });
