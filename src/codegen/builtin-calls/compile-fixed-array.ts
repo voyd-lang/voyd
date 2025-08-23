fea14fc5b0cfe3b80a911d02699792974d497020
Drew Youngwerth <drew@youngwerth.com>
2025-08-23 12:24:52 -0700

    Cast FixedArray arguments to union element type

--- src/codegen/builtin-calls/compile-fixed-array.ts
+++ src/codegen/builtin-calls/compile-fixed-array.ts
@@ -5,9 +5,18 @@ import * as gc from "../../lib/binaryen-gc/index.js";
 
 export const compileFixedArray = (opts: CompileExprOpts<Call>) => {
   const type = opts.expr.type as FixedArrayType;
+  const elemType = type.elemType!;
+  const elemBinaryenType = mapBinaryenType(opts, elemType);
+  const values = opts.expr.argArrayMap((expr) => {
+    const compiled = compileExpression({ ...opts, expr });
+    return elemType.isRefType()
+      ? gc.refCast(opts.mod, compiled, elemBinaryenType)
+      : compiled;
+  });
+  const arrayType = mapBinaryenType(opts, type);
   return gc.arrayNewFixed(
     opts.mod,
-    gc.binaryenTypeToHeapType(mapBinaryenType(opts, type)),
-    opts.expr.argArrayMap((expr) => compileExpression({ ...opts, expr }))
+    gc.binaryenTypeToHeapType(arrayType),
+    values
   );
 };
