53b3a1c496baf054411aa61076966bcafd44b189
Drew Youngwerth <drew@youngwerth.com>
2025-08-23 15:49:38 -0700

    Add test for array of objects (#204)

---
+++ src/__tests__/array-of-objects.e2e.test.ts
@@ -0,0 +1,20 @@
+import { arrayOfObjectsVoyd } from "./fixtures/array-of-objects.js";
+import { compile } from "../compiler.js";
+import { describe, test, beforeAll } from "vitest";
+import assert from "node:assert";
+import { getWasmFn, getWasmInstance } from "../lib/wasm.js";
+
+describe("E2E array of objects type inference", () => {
+  let instance: WebAssembly.Instance;
+
+  beforeAll(async () => {
+    const mod = await compile(arrayOfObjectsVoyd);
+    instance = getWasmInstance(mod);
+  });
+
+  test("run accepts Array<JsonNumber>", (t) => {
+    const fn = getWasmFn("run", instance);
+    assert(fn, "Function exists");
+    t.expect(fn(), "run returns correct value").toEqual(1);
+  });
+});
