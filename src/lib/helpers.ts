afab3d7dd90015760863d1b60b89a0ec973f2059
Drew Youngwerth <drew@youngwerth.com>
2025-08-24 12:24:48 -0700

    Remove unused helper

--- src/lib/helpers.ts
+++
@@ -1,7 +0,0 @@
-export function isCyclic(obj: any, count = 0): boolean {
-  if (count > 75) return true;
-  if (!obj) return false;
-  const parent = obj.parent;
-  if (parent) return isCyclic(parent, count + 1);
-  return false;
-}
