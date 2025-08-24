c5c12d595f3a0f2ac460cc465bdbfaa5d0a6bd73
Drew Youngwerth <drew@youngwerth.com>
2025-08-24 02:05:41 -0700

    Handle union array elements (#215)
    * test: add html array e2e
    
    * Fix html array test to count all strings

---
+++ src/__tests__/fixtures/html-array.ts
@@ -0,0 +1,28 @@
+export const htmlArrayVoyd = `
+use std::all
+
+obj Box { val: i32 }
+
+type Html = Array<Html> | String
+
+fn work(html: Array<Html>, sum: i32) -> i32
+  let it = html.iterate()
+  let reducer: (sum: i32) -> i32 = (sum: i32) -> i32 =>
+    it.next().match(opt)
+      Some<Html>:
+        opt.value.match(json)
+          Array<Html>:
+            reducer(work(json, sum))
+          String:
+            reducer(1 + sum)
+      None:
+        sum
+  reducer(sum)
+
+pub fn main() -> i32
+  work([
+    "hello",
+    "world",
+    ["how", "are", "we"]
+  ], 0)
+`;
