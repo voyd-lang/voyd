84c38435f951219b7e4dc2cadf0bb7c568598b14
Drew Youngwerth <drew@youngwerth.com>
2025-08-25 12:03:13 -0700

    feat: add basic map encoding (#239)

--- src/__tests__/fixtures/msg-pack.ts
+++ src/__tests__/fixtures/msg-pack.ts
@@ -1,6 +1,8 @@
 export const msgPackVoyd = `
 use std::msg_pack
 use std::linear_memory
+use std::map
+type MiniJson = Map<MiniJson> | Array<MiniJson> | String
 
 pub fn run_i32() -> i32
   msg_pack::encode_json(42, 0)
@@ -10,4 +12,9 @@ pub fn run_string() -> i32
 
 pub fn run_array() -> i32
   msg_pack::encode_json(["hey", "there"])
+
+pub fn run_map() -> i32
+  let m = new_map<MiniJson>()
+  m.set("hello", "world")
+  msg_pack::encode_json(m)
 `;
