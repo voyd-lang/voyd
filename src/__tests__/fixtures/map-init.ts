17ecdfc3b36723280df4cf543a868fe32ebb8b3d
Drew Youngwerth <drew@youngwerth.com>
2025-08-25 19:33:38 -0700

    feat: init map from key-value tuples

--- src/__tests__/fixtures/map-init.ts
+++ src/__tests__/fixtures/map-init.ts
@@ -2,11 +2,10 @@ export const mapInitVoyd = `
 use std::all
 
 pub fn map_from_pairs() -> i32
-  let bucket = new_array<{ key: String, value: i32 }>({ with_size: 1 })
-  bucket.push({ key: "a", value: 1 })
-  let buckets = new_array<Array<{ key: String, value: i32 }>>({ with_size: 1 })
-  buckets.push(bucket)
-  let m = Map<i32>(buckets)
+  let pairs = new_array<(String, i32)>({ with_size: 2 })
+  pairs.push(("a", 1))
+  pairs.push(("b", 2))
+  let m = Map<i32>(pairs)
   m.get("a").match(v)
     Some<i32>:
       v.value
