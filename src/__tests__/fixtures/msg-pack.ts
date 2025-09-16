046cf698f7374193a448c513fbd7173c263ecdcd
Drew Youngwerth <drew@youngwerth.com>
2025-09-15 23:19:35 -0700

    Add MsgPack decoder and tests

--- src/__tests__/fixtures/msg-pack.ts
+++ src/__tests__/fixtures/msg-pack.ts
@@ -18,4 +18,19 @@ pub fn run_map() -> i32
   m.set("hey", "there")
   m.set("goodbye", "night")
   msg_pack::encode(m)
+
+pub fn decode_i32(ptr: i32, len: i32) -> i32
+  msg_pack::decode_i32(ptr, len)
+
+pub fn decode_string(ptr: i32, len: i32) -> i32
+  let value = msg_pack::decode_string(ptr, len)
+  msg_pack::encode(value, 0)
+
+pub fn decode_array(ptr: i32, len: i32) -> i32
+  let array = msg_pack::decode_array(ptr, len)
+  msg_pack::encode(array)
+
+pub fn decode_map(ptr: i32, len: i32) -> i32
+  let map = msg_pack::decode_map(ptr, len)
+  msg_pack::encode(map)
 `;
