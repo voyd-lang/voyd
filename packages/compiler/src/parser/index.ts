0fe0242fe85d8fa2cc1238a148324bc68b2d6b5d
Drew Youngwerth <drew@youngwerth.com>
2026-03-30 13:18:35 -0700

    Release process tweaks (#607)

--- packages/compiler/src/parser/index.ts
+++ packages/compiler/src/parser/index.ts
@@ -2,4 +2,3 @@ export * from "./ast/index.js";
 export * from "./parser.js";
 export * from "./attributes.js";
 export * from "./errors.js";
-export { BENCH_FILE, BENCH_FILE_SM } from "./fixtures/benchmark-file.js";
